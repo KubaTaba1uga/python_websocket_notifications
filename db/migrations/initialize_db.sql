0bef328f31aff93eaa884dc6751c2672a7b0258b
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-09-19 12:29:50 +0200

    Create db service (#2)
    * add sql files
    
    * migrate initial data
    
    * add docker-compose

---
+++ db/migrations/initialize_db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE app_user (
+ id SERIAL PRIMARY KEY,
+ username TEXT NOT NULL
+);
+
+-- GRANT select, insert, update, delete on app_user to username ;
+
+CREATE TABLE message (
+     id SERIAL PRIMARY KEY,
+     content TEXT NOT NULL,
+     from_ INT NOT NULL,
+     to_ INT NOT NULL,
+     CONSTRAINT fk_from FOREIGN KEY(from_) REFERENCES app_user(id)
+);
+
+INSERT INTO app_user (username) VALUES
+       ('Jakub'), ('Maciek'), ('Magda'), ('Kuba'), ('Daniel');
+
+
+INSERT INTO message (content, from_, to_) VALUES
+       ('Czesc Kuba', 1, 2),
+       ('Czesc Maciek', 2, 1),
+       ('Lubie wszystko', 4, 3),
+       ('Nie lubie nic', 3, 4),
+       ('Kocham kotki', 5, 3),
+       ('Kocham foki', 1, 5);
+
